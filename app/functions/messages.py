3dcec612864d2b618888e8ba9b2ce2f5f94ea3a5
0xsysr3ll <0xsysr3ll@pm.me>
2023-09-12 18:52:45 +0200

    [fix] Does not display infos of user when no report count yet

--- app/functions/messages.py
+++ app/functions/messages.py
@@ -16,10 +16,13 @@ def user_infos(user):
     else:
         try:
             feed = ywh.get_user_feed(user)
-            bug = feed[0]["report"]["bug_type"]["name"]
+            
         except:
             return
-        
+        try:
+            bug = feed[0]["report"]["bug_type"]["name"]
+        except:
+            bug = "No reports yet"
         infos = ywh.get_user_infos(user)
         rank = infos["rank"]
         point = infos["points"]
